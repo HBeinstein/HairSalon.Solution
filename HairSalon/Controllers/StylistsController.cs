1480a15ed0582b6cbf0ee712878c8d49866cea4b
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-31 16:18:21 -0700

    add delete.cshtml file in stylists directory and add delete functionality to StylistController.cs

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -41,7 +41,19 @@ namespace HairSalon.Controllers
         return View(thisStylist);
     }
 
+    public ActionResult Delete(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+      return View(thisStylist);
+    }
 
-
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
